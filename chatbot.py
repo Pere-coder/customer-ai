287445d1f018f0576722da022493f3ae67f068fc
Pere-coder <gpere800@gmail.com>
2024-12-22 04:50:21 +0100

    first commit

---
+++ chatbot.py
@@ -0,0 +1,52 @@
+import requests
+from dotenv import load_dotenv
+import json
+import streamlit as st
+import os
+load_dotenv()
+BASE_API_URL = "https://api.langflow.astra.datastax.com"
+LANGFLOW_ID = "dbd771ab-68e9-4a58-9cd1-f65f88a62dee"
+FLOW_ID = "10bb5548-34dd-4568-acd2-dae96a7b0f8e"
+APPLICATION_TOKEN = os.environ.get("APP_TOKEN")
+ENDPOINT = "customer" # The endpoint name of the flow
+
+
+def run_flow(message: str) -> dict:
+    api_url = f"{BASE_API_URL}/lf/{LANGFLOW_ID}/api/v1/run/{ENDPOINT}"
+
+    payload = {
+        "input_value": message,
+        "output_type": "chat",
+        "input_type": "chat",
+    }
+    
+    headers = {"Authorization": "Bearer " + APPLICATION_TOKEN, "Content-Type": "application/json"}
+    response = requests.post(api_url, json=payload, headers=headers)
+    return response.json()
+
+
+def main():
+    st.title("Chat Interface")
+    message = st.text_area("Message", placeholder="Ask something...")
+
+    if st.button("Run Flow"):
+        if not message.strip():
+            st.error("Please enter a message")
+            return
+
+        try:
+            with st.spinner("Running flow..."):
+                response = run_flow(message)
+
+            response = response["outputs"][0]["outputs"][0]["results"]["message"]["text"]
+            print(response)
+            st.markdown(response)
+        except Exception as e:
+            st.error(str(e))
+
+if __name__ == "__main__":
+    main()
+
+
+
+
